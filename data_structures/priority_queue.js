6e57e55115e45c5090b11ae7460fb2356d9fb607
Aqib Shah <ashah023@fiu.edu>
2017-02-21 00:43:51 -0500

    WIP: Add boilerplate for priority queue

---
+++ data_structures/priority_queue.js
@@ -0,0 +1,16 @@
+module.exports = function PriorityQueue() {
+
+  const _size = 0;
+  const data = [];
+
+  return {
+    isEmpty: () => _size === 0,
+    size: () => _size,
+    getMin: () => { },
+    getMax: () => { },
+    removeMin: () => { },
+    removeMax: () => { },
+    put: () => { }
+  };
+
+};
